2f967fb445c55346eca41495ccff546b6bf55df1
brend3n <brenden@knights.ucf.edu>
2022-05-20 18:36:10 -0400

    Queue is working with basic functionality

--- src/data_structs/queue/queue_linked_list.c
+++ src/data_structs/queue/queue_linked_list.c
@@ -4,81 +4,105 @@
 
 struct Node{
 	int data;
-	struct Node* next;
 	int id;
+	struct Node* next;
+	
 }typedef Node;
 
 struct Queue{
 	int size;
+	int capacity;
 	Node* front;
 	Node* next_to_fill;
-	int capacity;
 }typedef Queue;
 
 struct Queue* init_queue(int size_s){
+
+	int id_val = 0;
+
 	// Allocate space for queue
-	struct Queue* q = malloc(1 * sizeof(Queue*));
+	struct Queue* q = malloc(sizeof(Queue*));
 	q->size = size_s;
 	q->front = malloc(sizeof(Node*));
-	q->next_to_fill(malloc(sizeof(Node*));
+	q->front->next = NULL;
+	q->next_to_fill = malloc(sizeof(Node*));
+	q->next_to_fill->next = NULL;
 
 	
-	// Init first node
-	Node* curr_node = malloc(1 * sizeof(Node*));
+	// Init head node
+	Node* curr_node = malloc(sizeof(Node*));
 	curr_node->next = NULL;
 	curr_node->data = -1;
-	curr_node->id = 0:
-
-	// init nodes in list of size= 
-	while (curr_node->id != (q->size-1))
-	{
-		// Init new node
-		Node* temp_node = malloc(1 * sizeof(Node*));
-		temp_node->next = NULL;
-		temp_node->data = -1;
-		temp_node->id = curr_node->id + 1;
-	
-		// Set next node
-		curr_node->next = temp_node;
-
-		// Update current node
-		curr_node = temp_node;
-	}
+	curr_node->id = id_val++;
 
 	// Set the front of the queue
 	q->front = q->next_to_fill = curr_node;
 
-	return new_q;
+	// Init new node
+	Node* temp = q->front;
+
+	// init nodes in list of size= 
+	int counter = 0;
+	while(counter < (size_s - 1)){
+
+		temp->next = malloc(sizeof(Node*));
+		temp->next->data = -1;
+		temp->next->id = id_val++;
+		temp = temp->next;
+		counter++;
+	}
+
+
+	return q;
 }
 
 // Insert into the back of the queue
 void enq(Queue* q, int data){
+	
+	q->next_to_fill->data = data;
+	q->capacity++;
+	q->next_to_fill = q->next_to_fill->next; 	
 
-	if(q->capacity < q->size){
-		q->next_to_fill->data = data;
-		q->capacity++;
-		
-		Node*temp = 	
-	}else{;}
 	return;
 }
 
 // Pop the first element of the queue
 int deq(Queue* q){
 
+	Node* temp = q->front;
 	// Get the element
 	int element = q->front->data;
+	q->front = q->front->next;
+	q->capacity--;
 
-	// Move back the front of the queue
-//	TODO
-	// Free the hanging node from the list
-//	TODO
+	free(temp);
 	return element;
+}
+
+void print_queue_info(Queue* q){
+	printf("q size: %d\n", q->size);
+	printf("q capacity: %d\n", q->capacity);
+	
+	Node* temp = q->front;
+	printf("Data: ");
+	while(temp != NULL){
+		printf("%d%s", temp->data, " ");
+		temp = temp->next;
+	}
+	printf("\n");
 }
 
 
 void main(){
 	Queue* q = init_queue(3);
-
+	print_queue_info(q);
+	enq(q,3);
+	print_queue_info(q);
+	enq(q,2);
+	enq(q,4);
+	print_queue_info(q);
+	deq(q);
+	print_queue_info(q);
+	
 	return;
 }
