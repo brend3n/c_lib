7482a13662fd25b96b3ec097b71ca5a6991b1edd
brend3n <brenden@knights.ucf.edu>
2022-05-19 21:42:04 -0400

    Need to fix queue

--- src/data_structs/queue/queue_linked_list.c
+++ src/data_structs/queue/queue_linked_list.c
@@ -11,22 +11,26 @@ struct Node{
 struct Queue{
 	int size;
 	Node* front;
-	Node* end;
-	int is_full;
+	Node* next_to_fill;
+	int capacity;
 }typedef Queue;
 
 struct Queue* init_queue(int size_s){
 	// Allocate space for queue
 	struct Queue* q = malloc(1 * sizeof(Queue*));
-	int size = size_s;
+	q->size = size_s;
+	q->front = malloc(sizeof(Node*));
+	q->next_to_fill(malloc(sizeof(Node*));
+
 	
 	// Init first node
 	Node* curr_node = malloc(1 * sizeof(Node*));
 	curr_node->next = NULL;
 	curr_node->data = -1;
-	curr_node->id = 0;
+	curr_node->id = 0:
+
 	// init nodes in list of size= 
-	while (curr_node->id != (size-1))
+	while (curr_node->id != (q->size-1))
 	{
 		// Init new node
 		Node* temp_node = malloc(1 * sizeof(Node*));
@@ -41,17 +45,40 @@ struct Queue* init_queue(int size_s){
 		curr_node = temp_node;
 	}
 
-	// TODO: Need to still set these
-	q->front = NULL;
-	q->end = NULL;
-
+	// Set the front of the queue
+	q->front = q->next_to_fill = curr_node;
 
 	return new_q;
 }
 
+// Insert into the back of the queue
+void enq(Queue* q, int data){
+
+	if(q->capacity < q->size){
+		q->next_to_fill->data = data;
+		q->capacity++;
+		
+		Node*temp = 	
+	}else{;}
+	return;
+}
+
+// Pop the first element of the queue
+int deq(Queue* q){
+
+	// Get the element
+	int element = q->front->data;
+
+	// Move back the front of the queue
+//	TODO
+	// Free the hanging node from the list
+//	TODO
+	return element;
+}
 
 
 void main(){
+	Queue* q = init_queue(3);
 
 	return;
 }
